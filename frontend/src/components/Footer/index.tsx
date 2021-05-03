000e3103c5ff14205ce3521e7ebe273c795f7b19
Jizreel Guimarães <jizreel9310@gmail.com>
2021-05-03 11:03:00 -0400

    implementações

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,16 @@
+const Footer = () => {
+    return (
+        <div >
+            <footer className="footer mt-auto py-3 bg-dark">
+                <div className="container">
+                    <p className="text-light">App desenvolvido por <a href="https://github.com/jizreelguimaraes" target="_blank" rel="noreferrer">Jizreel Guimarães</a></p>
+                    <p className="text-light"><small><strong>Semana Spring React</strong><br />
+      Evento promovido pela escola DevSuperior: <a href="https://www.linkedin.com/in/jizreelguimaraes/" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+                </div>
+            </footer>
+        </div>
+    );
+}
+
+export default Footer;
+
